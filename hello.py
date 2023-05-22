ce5840544ed750aaadd9da4d982c113c67638064
jpalex <aluno80433@ipt.pt>
2023-05-22 15:03:52 +0100

    First commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+def say_hi():
+    print('Hi, my name is JP.')
+
+def city():
+    print("Abrantes")
+
+__version__ = '1.2'
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='JPA',
+      author_email='myEmail@gmail.com',
+      url='The URL project - github for example',
+      py_modules=['hello'],
+     )
